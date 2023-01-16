d6373e1e34feeda80abfb449b3d00ff6459a81da
Dustin Stacy <dustin.stacy@outlook.com>
2023-01-16 18:34:12 -0500

    Add Image assets. Add Prompt collection. Add Active prompt styling

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -7,5 +7,63 @@ import blacklight from "./blacklight.png";
 import backlight from "./backlight.png";
 import candlelight from "./candlelight.png";
 import studiolight from "./studiolighting.png";
+import threeDRender from "./3drender.png";
+import serene from "./serene.png";
+import bokeh from "./bokeh.png";
+import closeup from "./close-up.png";
+import cybernetic from "./cybernetic.png";
+import gloomy from "./gloomy.png";
+import gothic from "./gothic.png";
+import longshot from "./longshot.png";
+import ominous from "./ominous.png";
+import overhead from "./overhead.png";
+import postapocalyptic from "./post-apocalyptic.png";
+import spirited from "./spirited.png";
+import vaporwave from "./vaporwave.png";
+import dali from "./dali.png";
+import experimental from "./experimental.png";
+import frazetta from "./frazetta.png";
+import giger from "./giger.png";
+import monumental from "./monumental.png";
+import organic from "./organic.png";
+import steadman from "./steadman.png";
+import airbrush from "./airbrush.png";
+import elaborate from "./elaborate.png";
+import etching from "./etching.png";
+import disney from "./disney.png";
 
-export { neondice, wrench, buildingBlocks, logo, loader, backlight, blacklight, candlelight, studiolight };
+export {
+  neondice,
+  wrench,
+  buildingBlocks,
+  logo,
+  loader,
+  backlight,
+  blacklight,
+  candlelight,
+  studiolight,
+  threeDRender,
+  serene,
+  bokeh,
+  closeup,
+  cybernetic,
+  gloomy,
+  gothic,
+  longshot,
+  ominous,
+  overhead,
+  postapocalyptic,
+  spirited,
+  vaporwave,
+  dali,
+  experimental,
+  frazetta,
+  giger,
+  monumental,
+  organic,
+  steadman,
+  airbrush,
+  elaborate,
+  disney,
+  etching,
+};
--- client/src/components/PromptCard/PromptCard.jsx
+++ client/src/components/PromptCard/PromptCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import './PromptCard.css'
 
-const PromptCard = ({ title, image, details, handleClick}) => {
+const PromptCard = ({ title, image, details, handleClick, value}) => {
   return (
-    <div className="prompt__card"onClick={handleClick}>
+    <div className="prompt__card"onClick={handleClick} value={value}>
       <h4 className='prompt__card__title'>{title}</h4>
       <img src={image} alt={title} className='prompt__card__image'/>
       <p className='prompt__card__text'>{details}</p>
